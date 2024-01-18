56a1b68e17658ccd17f04b6434df4bd53981cd87
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-01-19 00:47:33 +0300

    Safe divide

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,20 @@
+-- Creates a function SafeDiv
+-- It divides the first by the second number
+-- Returns the result
+DELIMITER //
+CREATE FUNCTION SafeDiv(
+	a INT,
+	b INT
+)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+	DECLARE result FLOAT;
+	IF b = 0 THEN
+		RETURN 0;
+        END IF;
+        SET result = (a * 1.0) / b;
+        RETURN result;
+END;
+//
+DELIMITER ;

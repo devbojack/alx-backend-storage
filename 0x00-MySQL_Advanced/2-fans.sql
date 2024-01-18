24a70bd7e6527725cca61a03391f24ddb079b5a9
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-01-18 23:23:49 +0300

    Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,6 @@
 -- Ranks country origins of bands
 -- Order by number of fans
-SELECT origin, SUM(fans) as nb_fans from metal_bands
-GROUP BY origin ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) as nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;

127b72224b424b3448c3491429c9795215eab608
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-01-18 22:27:05 +0300

    We are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Creates user table if it does not exist
 -- id, email, name attributes
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)

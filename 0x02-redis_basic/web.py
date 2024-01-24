0054eab9e3e178869d58347baef62cc76a8d2e4c
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-01-24 21:16:08 +0300

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,62 +1,34 @@
 #!/usr/bin/env python3
-"""Implementing an expiring web cache and tracker"""
+""" expiring web cache module """
+
 import redis
 import requests
 from typing import Callable
 from functools import wraps
 
-redis_conn = redis.Redis()
+redis = redis.Redis()
 
 
-def track_access_count(fn: Callable) -> Callable:
-    """ Decorator to track the access count of a URL """
-
-    @wraps(fn)
-    def wrapper(url):
-        """ Wrapper for decorator """
-        redis_conn.incr(f"count:{url}")
-        return fn(url)
-
-    return wrapper
-
-
-def cache_page(fn: Callable) -> Callable:
-    """
-    Decorator to cache the result of func with a specified expiration time
-    """
+def wrap_requests(fn: Callable) -> Callable:
+    """ Decorator wrapper """
 
     @wraps(fn)
     def wrapper(url):
-        """ Wrapper for decorator """
-        key = f"cached:{url}"
-
-        # Check if the result is already in the cache
-        cached_response = redis_conn.get(key)
+        """ Wrapper for decorator guy """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
         if cached_response:
             return cached_response.decode('utf-8')
-
-        # If not in the cache, call the function and cache the result
         result = fn(url)
-        redis_conn.setex(key, 10, result)
+        redis.setex(f"cached:{url}", 10, result)
         return result
 
     return wrapper
 
 
-@track_access_count
-@cache_page
+@wrap_requests
 def get_page(url: str) -> str:
-    """Obtain the HTML content of a particular URL and returns it"""
+    """get page self descriptive
+    """
     response = requests.get(url)
     return response.text
-
-
-# Example usage:
-url = "http://slowwly.robertomurray.co.uk"
-html_content = get_page(url)
-print(html_content)
-
-# Check access count for the URL
-access_count_key = f"count:{url}"
-access_count = redis_conn.get(access_count_key)
-print(f"{url} was accessed {access_count} times.")
